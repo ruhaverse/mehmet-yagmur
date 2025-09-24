83d62ea239fa891689abb0725b82014ca9481e71
RUHAVERSE <firatshareupx@gmail.com>
2025-09-24 10:16:52 +0000

    Fix README issues and prepare for deployment

---
+++ MehmetYagmurApp/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-gesture-handler|@babel|react-native/jest|react-native-web|react-native-config|react-test-renderer|@expo|expo)/)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      babelConfig: '<rootDir>/babel.config.js',
+    },
+  },
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
---
+++ backend/services/media-service/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.set('trust proxy', true);
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'Media Service is running' });
+});
+
+const PORT = 3007;
+app.listen(PORT, () => {
+  console.log(`Media Service running on port ${PORT}`);
+});
